cde65cbad8a7ae2e248084bca1b99c2f4cced135
Mihika Sarwate <mihika@example.com>
2026-04-20 10:18:11 +0530

    Deploy static export to GitHub Pages - fixed 404

--- sanity/lib/client.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from "next-sanity";
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "w6jeuoaf",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-10-01",
-  useCdn: false,
-});
